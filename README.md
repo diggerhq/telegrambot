e445f2ea49e7246e5ef9f2ad5440c7d43fec2cf4
Igor Zalutski <izalutski@gmail.com>
2022-06-28 22:19:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# telegrambot
+
+Telegram bot example by Digger
+
+# Registering your bot with Telegram
+1. Add BotFather in Telegram
+2. type `/newbot` and follow instructions
+
+# Running locally
+
+```
+export BOT_TOKEN=<your_telegram_bot_token>
+```
+```
+node index.js
+```
+
+# Deploying
+
+Make sure to set the `BOT_TOKEN` environment variable
---
+++ index.js
@@ -0,0 +1,29 @@
+import { Telegraf } from 'telegraf';
+import express from 'express';
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const server = express();
+
+// health check
+server.get('/', (req, res) => {
+  res.send('Telegram Bot Example by Digger - OK');
+});
+
+server.listen(8080);
+
+bot.start((ctx) => ctx.reply(`
+Hi there! I'm Telegram Bot Example by Digger
+`));
+
+bot.command('helloworld', (ctx) => {
+  ctx.reply('Hello World!');
+});
+
+bot.on('message', async (ctx) => {
+  ctx.reply('Message received!');
+});
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
